10900aed86d05e2bfc6c600fb2551c1f5b2e3b19
Pranav Aggarwal <pranav.aggarwal2004@gmail.com>
2026-01-22 00:49:47 +0530

    Backend URL Hosted With AWS Succesfully

--- client/src/utils/getBackendUrl.js
+++ client/src/utils/getBackendUrl.js
@@ -1,4 +1,20 @@
 const getBackendUrl = () => {
+  const isLocalhost = window.location.hostname === "localhost";
+  return isLocalhost
+    // ? "http://localhost:5000"          //  Local Development Server
+    ? "http://13.234.204.195:5000/"       //! AWS Cloud HOSTED SERVER 
+    : "https://ipo-s-live-tracker.onrender.com";      //  Render.com Hosted Server
+};  
+
+export default getBackendUrl;
+
+
+// ! AWS Cloud HOSTED SERVER Backend Links
+// http://13.234.204.195:5000/
+// http://13.234.204.195:5000/api/ipos
+
+/*
+const getBackendUrl = () => {
   const host = window.location.hostname;
 
   if (host === "localhost") {
@@ -11,22 +27,4 @@ const getBackendUrl = () => {
 
   return "https://ipo-s-live-tracker.onrender.com";
 };
-
-export default getBackendUrl;
-
-
-// ! AWS Cloud HOSTED SERVER Backend Links
-// http://13.234.204.195:5000/
-// http://13.234.204.195:5000/api/ipos
-
-/*
-const getBackendUrl = () => {
-  const isLocalhost = window.location.hostname === "localhost";
-  return isLocalhost
-    ? "http://localhost:5000"
-    ? "http://13.234.204.195:5000/"
-    : "https://ipo-s-live-tracker.onrender.com";      // Live Deployed Hosted Server
-};  
-
-export default getBackendUrl;
 */
